b0a317c211df1e896b09672e170738523c74b6b9
sliceofcode <cheesy.fried.bacon@gmail.com>
2017-11-05 17:36:43 -0800

    simplify/add more defaults

--- scripts/defaults.sh
+++ scripts/defaults.sh
@@ -1,35 +1,45 @@
 #!/usr/bin/bash
 
+w() {
+  echo ">> defaults write" "$@"
+  defaults write "$@"
+}
+
 # dock
-defaults write com.apple.dock tilesize -int 36
-defaults write com.apple.dock magnification -float 0
-defaults write com.apple.dock mineffect -string "scale"
+w com.apple.dock tilesize -int 36
+w com.apple.dock magnification -float 0
+w com.apple.dock mineffect -string "scale"
 
 # finder
-defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+w com.apple.finder FXEnableExtensionChangeWarning -bool false
 
 # spaces
-defaults write com.apple.dock mru-spaces -bool false
-defaults write com.apple.dock dashboard-in-overlay -bool true
-defaults write com.apple.dashboard mcx-disabled -bool true
-defaults write com.apple.dock expose-group-by-app -bool true
+w com.apple.dock mru-spaces -bool false
+w com.apple.dock dashboard-in-overlay -bool true # disable dashboard
+w com.apple.dashboard mcx-disabled -bool true # disable dashboard
+w com.apple.dock expose-group-by-app -bool true
 
 # key repeat
-defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
-defaults write NSGlobalDomain KeyRepeat -int 3
-defaults write NSGlobalDomain InitialKeyRepeat -int 20
+w -g ApplePressAndHoldEnabled -bool false # ditch accents for key repeat
+w -g KeyRepeat -int 2
+w -g InitialKeyRepeat -int 20
+w -g AppleFontSmoothing -int 2 # lighter font smoothing
 
 # keyboard
-defaults write NSGlobalDomain AppleKeyboardUIMode -int 2
+w -g AppleKeyboardUIMode -int 2 # fka
 
 # autohide
-defaults write com.apple.dock autohide-delay -float 0
-defaults write com.apple.dock autohide-time-modifier -float 0.8
-defaults write com.apple.dock autohide -bool true
-defaults write com.apple.dock showhidden -bool true
+w com.apple.dock autohide-delay -float 0
+w com.apple.dock autohide-time-modifier -float 0.8
+w com.apple.dock autohide -bool true
+w com.apple.dock showhidden -bool true
+
+# iterm
+w com.googlecode.iterm2 PromptOnQuit -bool false
+w com.googlecode.iterm2 QuitWhenAllWindowsClosed -bool false
 
 # terminal
-defaults write com.apple.terminal SecureKeyboardEntry -bool true
+w com.apple.terminal SecureKeyboardEntry -bool true
 
 # accessibility
-defaults write com.apple.universalaccess reduceMotion -bool true
+w com.apple.universalaccess reduceMotion -bool true
