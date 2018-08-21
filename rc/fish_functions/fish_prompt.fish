ed414345263307cd9c5ab9ccd07a3a0be889c966
slice <ryaneft@gmail.com>
2018-08-20 22:00:47 -0700

    useless fish update

--- rc/fish_functions/fish_prompt.fish
+++ rc/fish_functions/fish_prompt.fish
@@ -5,15 +5,15 @@ function fish_prompt
 
   echo
 
-  set -l hostname (hostname | cut -d . -f 1)
+  set -l hn (hostname | cut -d . -f 1)
 
   printf '%s%s%s@%s%s %s%s%s\n' \
     (set_color normal) (whoami) \
-    (set_color brblack) $hostname (set_color normal) \
+    (set_color brblack) $hn (set_color normal) \
     (set_color red) (prompt_pwd) (set_color normal)
 
   set time_string ""
-  if [ $CMD_DURATION != 0 ]
+  if test "$CMD_DURATION" != ""
     set -l hours (math "$CMD_DURATION / 3600000")
     set -l minutes (math "$CMD_DURATION % 3600000 / 60000")
     set -l seconds (math "$CMD_DURATION % 60000 / 1000")
@@ -23,7 +23,7 @@ function fish_prompt
     [ $minutes -gt 0 ]; and set pretty {$pretty}{$minutes}'m'
     [ $seconds -gt 0 ]; and set pretty {$pretty}{$seconds}'s'
 
-    [ $seconds -gt 3 -o $minutes -gt 0 -o $hours -gt 0 ]
+    test $seconds -gt 3 -o $minutes -gt 0 -o $hours -gt 0
     and set time_string (set_color -o f74782)"($pretty) "(set_color normal)
     or set time_string ""
   end
