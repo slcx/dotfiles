20cf15d4bd790c9212b9c50b0241f2d32d7d59ef
slice <ryaneft@gmail.com>
2018-08-26 17:18:47 -0700

    remove fish

--- rc/fish_functions/gsu.fish
+++
@@ -1,9 +0,0 @@
-function gsu -d "Sets up a Git repository"
-  if [ (count $argv) -eq 0 ]
-    echo "usage: gsu <remote>"
-    return
-  end
-
-  git remote add origin argv[1]
-  git push -u origin master
-end
