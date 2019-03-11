c28740f3f07acc14e3f87f9ae7604a787fc9a8b7
slice <ryaneft@gmail.com>
2019-03-10 19:00:09 -0700

    make right prompt use printf

--- config/fish_functions/fish_right_prompt.fish
+++ config/fish_functions/fish_right_prompt.fish
@@ -4,8 +4,10 @@ function fish_right_prompt
   command_duration
 
   if test "$command_status" -eq 0
-    set_color green; echo -n "^_^"; set_color normal
+    printf '%s^_^%s' (set_color green) (set_color normal)
   else
-    set_color -o red; echo -n "O_o"; set_color normal
+    # set -l face (random choice 'O_O' 'O_o' '>_>' 'v_v' ';_;')
+    # printf '%s%s%s' (set_color -o red) "$face" (set_color normal)
+    printf '%s;_;%s' (set_color -o red) (set_color normal)
   end
 end
