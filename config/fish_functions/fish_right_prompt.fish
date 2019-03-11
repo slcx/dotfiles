ac99437c188f3c8eaf4fa6f5d1ecd637d0db68a2
slice <ryaneft@gmail.com>
2019-03-10 18:49:46 -0700

    fix shell always displaying ^_^

--- config/fish_functions/fish_right_prompt.fish
+++ config/fish_functions/fish_right_prompt.fish
@@ -1,7 +1,9 @@
 function fish_right_prompt
+  set -l command_status "$status"
+
   command_duration
 
-  if test "$status" -eq 0
+  if test "$command_status" -eq 0
     set_color green; echo -n "^_^"; set_color normal
   else
     set_color -o red; echo -n "O_o"; set_color normal
