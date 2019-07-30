7e1b83ea211d699f6eeb584e70e27507b10c0fcb
slice <ryaneft@gmail.com>
2019-07-30 15:33:20 -0700

    fish prompt: change ssh color, add root support

--- config/fish_functions/fish_prompt.fish
+++ config/fish_functions/fish_prompt.fish
@@ -1,13 +1,25 @@
 function fish_prompt
-  # output ssh connection information
+  # if we're in ssh, show username and hostname
   if set -q SSH_CONNECTION
     printf '%s%s%s@%s%s ' \
-      (set_color magenta) \
+      (set_color blue) \
       "$USER" \
-      (set_color -o magenta) \
+      (set_color -o blue) \
       (hostname -s) \
       (set_color normal)
   end
 
-  printf '%s%s$%s ' (prompt_pwd) (set_color -o) (set_color normal)
+  set prompt_character '$'
+  set prompt_color '-o'
+
+  if test "$USER" = "root"
+    set prompt_character '#'
+    set prompt_color red -o
+  end
+
+  printf '%s%s%s%s ' \
+    (prompt_pwd) \
+    (set_color $prompt_color) \
+    $prompt_character \
+    (set_color normal)
 end
