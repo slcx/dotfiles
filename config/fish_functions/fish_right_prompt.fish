1cfe52074348763e7a71ee0b5d63ce2fb8863d36
slice <ryaneft@gmail.com>
2019-08-12 17:32:01 -0700

    re-fix fish_right_prompt

--- config/fish_functions/fish_right_prompt.fish
+++ config/fish_functions/fish_right_prompt.fish
@@ -1,7 +1,11 @@
 function fish_right_prompt
+  # set this so we can compare the value pre-command_duration (which modifies
+  # it)
+  set -l _status "$status"
+
   command_duration
 
-  if test "$status" -eq 0
+  if test "$_status" -eq 0
     printf '%s^_^%s' (set_color green) (set_color normal)
   else
     # set -l face (random choice 'O_O' 'O_o' '>_>' 'v_v' ';_;')
