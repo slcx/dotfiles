0343719155beb8ca5ca95a9a84ce5cfd0ea832c5
slice <ryaneft@gmail.com>
2020-03-18 22:56:08 -0700

    fish_functions/spek: use sox instead of ffmpeg
    ffmpeg's spectrogram can be a bit finnicky

--- config/fish_functions/spek.fish
+++ config/fish_functions/spek.fish
@@ -1,9 +1,9 @@
 function spek
   set -l id (random)
-  set -l options 'showspectrumpic=s=1024x512:stop=22000'
   set -l path '/tmp/spectrogram-'(random)'.png'
 
-  echo 'generating spectogram...'
-  ffmpeg -v warning -y -i $argv[1] -filter_complex "$options" "$path"
+  echo "creating spek @ $path"
+
+  sox $argv[1] -n spectrogram -o "$path"
   and open "$path"
 end
