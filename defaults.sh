38d8aa4653262a8197d17759b7b4b1e4e6930726
slice <tinyslices@gmail.com>
2021-05-31 12:48:55 -0700

    update macOS defaults to 11.4

--- defaults.sh
+++ defaults.sh
@@ -1,17 +1,23 @@
-#!/bin/bash
+#!/bin/sh
+
+# Last tested with macOS 11.4
 
 w() {
-  echo "> defaults write" "$@"
+  echo "defaults write" "$@"
   defaults write "$@"
 }
 
 sw() {
-  echo "> sudo defaults write" "$@"
+  echo "sudo defaults write" "$@"
   sudo defaults write "$@"
 }
 
-w -g AppleFontSmoothing -int 1
-sw -g AppleFontSmoothing -int 1
+# Subpixel font anti-aliasing has been gone since macOS 10.14, but it should be
+# fine. The default font rendering settings seem to be fine now, so don't
+# bother changing them.
+
+# w -g AppleFontSmoothing -int 1
+# sw -g AppleFontSmoothing -int 1
 
 # dock
 w com.apple.dock tilesize -int 50
@@ -20,27 +26,22 @@ w com.apple.dock show-recents -bool no
 w com.apple.dock magnification -float 0
 w com.apple.dock mineffect -string "scale"
 
-# look up
-w com.apple.lookup.shared LookupSuggestionsDisabled -bool yes
-
 # finder
-w com.apple.finder _FXSortFoldersFirst -bool true
+# I don't want to test this to see if it still works.
+# w com.apple.finder _FXSortFoldersFirst -bool true
 w com.apple.finder FXEnableExtensionChangeWarning -bool false
 w com.apple.finder FXDefaultSearchScope -string SCcf
 w com.apple.finder NewWindowTarget -string PfHm
 w com.apple.finder NewWindowPath -string "file:///Users/$(whoami)/"
-w com.apple.finder ShowRecentTags -bool false
 
 # spaces
 w com.apple.dock mru-spaces -bool false
-w com.apple.dock dashboard-in-overlay -bool true # disable dashboard
-w com.apple.dashboard mcx-disabled -bool true # disable dashboard
-w com.apple.dock expose-group-by-app -bool true
+w com.apple.dock expose-group-apps -bool true
 
 # key repeat
-w -g ApplePressAndHoldEnabled -bool false # ditch accents for key repeat
-w -g KeyRepeat -int 1
-w -g InitialKeyRepeat -int 20
+w -g ApplePressAndHoldEnabled -bool false
+w -g KeyRepeat -int 2
+w -g InitialKeyRepeat -int 15
 
 # keyboard
 w -g AppleKeyboardUIMode -int 2 # full keyboard access
@@ -58,7 +59,7 @@ w com.apple.dock autohide-time-modifier -float 0.4
 w com.apple.dock autohide -bool true
 w com.apple.dock showhidden -bool true
 
-# iterm
+# iterm2
 w com.googlecode.iterm2 PromptOnQuit -bool false
 w com.googlecode.iterm2 QuitWhenAllWindowsClosed -bool false
 w com.googlecode.iterm2 SplitPaneDimmingAmount -float 0.6
@@ -72,8 +73,5 @@ w com.googlecode.iterm2 HideTab -bool true
 w com.googlecode.iterm2 HideTabCloseButton -bool true
 w com.googlecode.iterm2 HideTabNumber -bool true
 
-# terminal
-w com.apple.terminal SecureKeyboardEntry -bool true
-
 # accessibility
-sw com.apple.universalaccess reduceMotion -bool true
+# sw com.apple.universalaccess reduceMotion -bool true
